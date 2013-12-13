bf3c0127d945802821cb82265d0aaa1906bf3edd
Sri Kiran Kanchibhotla <kvrs.kiran@gmail.com>
2013-12-12 20:29:25 -0500

    Updating settings.py

--- projGET/settings.py
+++ projGET/settings.py
@@ -12,18 +12,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        # 'USER': 'b561f13_skanchib',
-        # 'PASSWORD': 'kan10CHIBHOTLA',
-        # 'NAME': 'b561f13_skanchib',
-        # 'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.getcwd() + '/sqllite2.db',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        # 'USER': 'edison',
-        # 'PASSWORD': 'zhren123',
-        # 'HOST': '127.0.0.1',        
-        'HOST': 'silo.cs.indiana.edu',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '3306',                      # Set to empty string for default.
     }
 }
 
